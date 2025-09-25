0ac0c3a268ba18f2d3e630c76b39d1167544ad56
Szymon <szymonngos@gmail.com>
2025-09-25 21:27:50 +0100

    minor -bg stripes

--- src/components/BackgroundWrapper.tsx
+++ src/components/BackgroundWrapper.tsx
@@ -6,8 +6,8 @@ const BackgroundWrapper = () => {
           <div className="absolute inset-y-0 left-0 w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[fadeInScale_1.1s_ease-out_0.1s_forwards]" />
           <div className="absolute inset-y-0 left-10 w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[fadeInScale_1.1s_ease-out_0.3s_forwards]" />
           <div className="absolute inset-y-0 left-36 w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[fadeInScale_1.1s_ease-out_0.6s_forwards]" />
-          <div className="hidden md:absolute inset-y-0 right-[35%] w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[slideInFromTop_1.1s_ease-out_0.7s_forwards,livingBreathe5s_20s_ease-in-out_6s_infinite]" />
-          <div className="hidden md:absolute inset-y-0 right-10 w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[livingBreathe4s_20s_ease-in-out_6s_infinite]" />
+          <div className="hidden md:block absolute inset-y-0 right-[35%] w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[slideInFromTop_1.1s_ease-out_0.7s_forwards,livingBreathe5s_20s_ease-in-out_6s_infinite]" />
+          <div className="hidden md:block absolute inset-y-0 right-10 w-0 border-l-[0.5px] border-zinc-700 opacity-0 animate-[livingBreathe4s_20s_ease-in-out_6s_infinite]" />
         </div>
       </div>
     </>
