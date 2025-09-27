061bd2b72589baa22a1a59759683ac81a56c4050
Szymon <szymonngos@gmail.com>
2025-09-27 20:55:26 +0100

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/portfolio">
+  <BrowserRouter basename="/">
     <StrictMode>
       <App />
     </StrictMode>
