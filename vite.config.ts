10d7da49503dba77ae5d5586b5d5f411210ae58c
Szymon <szymonngos@gmail.com>
2025-09-27 20:17:54 +0100

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/szymongos.github.io",
   plugins: [react(), tailwindcss()],
 });
